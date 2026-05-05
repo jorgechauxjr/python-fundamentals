3a38b3cd544baffc77a668cd63425cb5b1f6e799
jorge <chauxjorge@hotmail.com>
2026-05-05 17:40:20 -0500

    [Add] 28 Módulos en Python: organizar código en múltiples archivos

---
+++ 0x010-modulos/main.py
@@ -0,0 +1,10 @@
+# import operaciones
+# print(operaciones.sumar(2, 3))
+
+from operaciones import sumar, restar, multiplicar, dividir
+
+print(sumar(2, 3))
+print(restar(2, 3))
+print(multiplicar(2, 3))
+print(dividir(10, 2))
+
---
+++ 0x010-modulos/operaciones.py
@@ -0,0 +1,51 @@
+def sumar(a: int, b: int):
+    """
+    Suma dos numeros
+
+    Args:
+        a (int): primer numero
+        b (int): segundo numero
+
+    Returns:
+        la suma de a + b
+    """
+    return a + b
+
+def restar(a: int, b: int):
+    """
+    Resta dos numeros
+
+    Args:
+        a (int): primer numero
+        b (int): segundo numero
+
+    Returns:
+        la resta de a - b
+    """
+    return a - b
+
+def multiplicar(a: int, b: int):
+    """
+    Multiplica dos numeros
+
+    Args:
+        a (int): primer numero
+        b (int): segundo numero
+
+    Returns:
+        la multiplicación de a * b
+    """
+    return a * b
+
+def dividir(a: int, b: int):
+    """
+    Divide dos numeros
+
+    Args:
+        a (int): primer numero
+        b (int): segundo numero
+
+    Returns:
+        la división de a / b
+    """
+    return a / b
